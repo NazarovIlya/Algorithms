568291d1462efaf8250d19a48d9f206925518f25
NazarovIlya <eliakohai@gmail.com>
2023-03-27 23:26:58 +0300

    repo initialized & BubbleSort added

---
+++ Sorts/Sorts/Program.cs
@@ -0,0 +1,11 @@
+﻿using Sorts;
+
+int[] arr = new int[] { 6, 3, 9, 1, 0, -10, 2, 7 };
+
+Console.WriteLine(string.Join(", ", arr));
+
+SortsInt.BubbleSort(arr);
+
+Console.WriteLine(string.Join(", ", arr));
+
+Console.ReadLine();
