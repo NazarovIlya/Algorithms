ac7fa7ff8f79f4cf67be629943786ecdd225cac7
NazarovIlya <eliakohai@gmail.com>
2023-03-31 09:57:33 +0300

    search initialized && direct search added

---
+++ Search/Search/Search.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+	internal class Search
+	{
+		public static int DirectSearch(int[] array, int value)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i] == value)
+					return i;
+			}
+			return -1;
+		}
+	}
+}
