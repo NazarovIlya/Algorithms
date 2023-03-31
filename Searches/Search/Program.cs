92a5313dfb662e898d4677257202370e8e331c24
NazarovIlya <eliakohai@gmail.com>
2023-03-31 10:07:26 +0300

    search initialized && direct search added

---
+++ Searches/Search/Program.cs
@@ -0,0 +1,9 @@
+﻿using Searches;
+
+Console.Clear();
+
+int[] arr = new int[] { 6, 3, 9, 1, 0, -10, 2, 7 };
+
+int result = Search.DirectSearch(arr, -10);
+Console.WriteLine($"Индекс найденного элемента: {result}");
+Console.ReadLine();
