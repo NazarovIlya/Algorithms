b821ca641a73bd5ed2875ee960ad8ead0dce95ba
NazarovIlya <eliakohai@gmail.com>
2023-04-08 11:29:57 +0300

    list utils init

---
+++ Lists/MyLists/MyLists/ListUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLists
+{
+	internal class ListUtils
+	{
+		public static void PrintList<T>(MyLinkedList<T> list)
+		{
+			var item = list.Head;
+
+			while (item != null)
+			{
+				Console.WriteLine(item);
+				item = item.Next;
+			}
+		}
+	}
+}
