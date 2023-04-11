6aec1e199253b1bde9641d60bfa8790a29ee8341
NazarovIlya <eliakohai@gmail.com>
2023-04-12 00:20:43 +0300

    trees classes init

---
+++ Trees/Trees/TreeNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+	internal class TreeNode<T>
+	{
+		public T Value { get; set; }
+		public List<TreeNode<T>> Children { get; set; }
+	}
+}
